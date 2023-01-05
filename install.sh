8835a03e81154c5bf7c5b415db8f9051ca70b73b
Alexey Vidanov <avidanov@tecracer.de>
2023-01-05 19:39:48 +0000

    Corrected again install /and .zshrc

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/hanjunlee/terragrunt-oh-my-zsh-plugin ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/terragrunt
-#cp ~/dotfiles/.zshrc ~/.zshrc
+cp ~/dotfiles/.zshrc ~/.zshrc
